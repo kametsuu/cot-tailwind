d4bb4743d8f659a05055f117907e7ce1adc629cb
kametsuu <itskametsu@gmail.com>
2025-06-21 22:55:44 +0200

    Revert "Use only `build.rs` instead"
    This reverts commit 595a1a82e5b508e7eda711abc0c1c5ac22353f53.

--- build.rs
+++ build.rs
@@ -1,16 +1,13 @@
-use std::process::Command;
+use std::fs;
+use std::path::Path;
 
 fn main() {
-    let status = Command::new("tailwindcss")
-        .args([
-            "-i", "static/css/tailwind.css",
-            "-o", "static/gen/main.css",
-        ])
-        .status();
-
-    match status {
-        Ok(status) if status.success() => println!("TailwindCSS compiled successfully."),
-        Ok(status) => eprintln!("TailwindCSS failed: {}", status),
-        Err(err) => eprintln!("Failed to run tailwindcss: {}", err),
+    let out_file = Path::new("static/gen/main.css");
+    if !out_file.exists() {
+        fs::create_dir_all(out_file.parent().unwrap()).unwrap();
+        fs::write(&out_file, "").unwrap();
     }
+
+    // Re-run build script if file is missing
+    println!("cargo:rerun-if-changed=build.rs");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod migrations;
+mod tailwindcss;
 
 use askama::Template;
 use cot::cli::CliMetadata;
@@ -9,7 +10,7 @@ use cot::project::{MiddlewareContext, RegisterAppsContext, RootHandlerBuilder};
 use cot::router::{Route, Router};
 use cot::static_files::{StaticFile, StaticFilesMiddleware};
 use cot::{App, AppBuilder, BoxedHandler, Project, static_files};
-
+use tailwindcss::TailwindMiddleware;
 
 
 #[derive(Debug, Template)]
@@ -39,7 +40,7 @@ impl App for CotTailwindcssApp {
     }
 
     fn static_files(&self) -> Vec<StaticFile> {
-        static_files!("gen/main.css")
+        static_files!("static/gen/main.css")
     }
 }
 
@@ -60,6 +61,7 @@ impl Project for CotTailwindcssProject {
         context: &MiddlewareContext,
     ) -> BoxedHandler {
         handler
+            .middleware(TailwindMiddleware::new())
             .middleware(StaticFilesMiddleware::from_context(context))
             .middleware(AuthMiddleware::new())
             .middleware(SessionMiddleware::new())
